9d2c56b360b37bf62336a0edce5437c70d06d72b
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2023-09-25 17:54:08 -0500

    added tsts

--- src/App.test.js
+++ src/App.test.js
@@ -12,3 +12,9 @@ test('renders learn react link 2', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link 3', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
